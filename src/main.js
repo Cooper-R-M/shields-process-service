769edf3ce96194237a1ba087e054a458609a72d7
Cooper Mcgaffey <lmcgaffey@cnm.edu>
2024-08-17 12:19:56 -0600

    working on contact section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import './index.css'
 import 'flowbite'
 
+document.querySelectorAll('.accordion-header').forEach(button => {
+    button.addEventListener('click', () => {
+        // Toggle the accordion content
+        const content = button.nextElementSibling;
+        content.classList.toggle('open');
 
+        // Optional: Close other open contents
+        document.querySelectorAll('.accordion-content').forEach(item => {
+            if (item !== content) {
+                item.classList.remove('open');
+            }
+        });
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,13 @@ module.exports = {
     extend: {
       colors: {
         yellow: '#F5Cc00',
+        vanilla: '#F4E9B4',
         black: '#0D1321',
         navy: '#05204A',
+        blue: '#345270',
         grey: '#628395',
+        french: '#ABBBC2',
+        white: '#F3F2EF',
       },
       rotate: {
         rotate: 'transform(90deg)',
